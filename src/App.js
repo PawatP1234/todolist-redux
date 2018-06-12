0f604a8b0b229f80ca384ba46f1a1f40d6413f44
Pawat <male.p@arms-asia.com>
2018-06-12 13:39:38 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <input type='text' placeholder='enter to do list...' />
+        <button>submit</button>
+      </div>
+    );
+  }
+}
+
+export default App;
